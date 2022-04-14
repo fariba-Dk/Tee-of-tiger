f51d182fbebb0f6a81ea143b6fc0b8f9c1fc3667
Fariba Dadko <faribacodes@gmail.com>
2022-04-13 21:15:46 -0700

    fixed react-router

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Routes,
+  Route,
+  Switch } from "react-router-dom";
+
+import './App.css';
+import Home from './routes/Home'
+import Posts from './routes/Posts'
+import UpdatePost from './routes/UpdatePost'
+import PostDetail from './routes/UpdatePost'
+
+
+function App() {
+  return (
+    <div>
+
+      <Routes>
+
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/posts" exact element={<Posts/>}/>
+          <Route path="/posts/:id/update" element={<UpdatePost/>}/>
+          <Route path="/posts/:id" element={<PostDetail/>}/>
+
+      </Routes>
+
+      </div>
+
+  );
+}
+
+export default App;
---
+++ client/src/routes/PostDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostDetail = () => {
+
+  return(
+    <div>
+    <h1>Post Detail</h1>
+
+    </div>
+  )
+}
+export default PostDetail
